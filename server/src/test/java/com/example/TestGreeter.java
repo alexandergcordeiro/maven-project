45e0e6be7593df347c4198081d99f39e84c65811
Alexander Cordeiro <alexandergcordeiro@gmail.com>
2020-06-04 21:11:21 -0300

    changed the jsp

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -31,9 +31,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-  @Test
-  public void dummyJenkinsTest() {
-    assertTrue(true);
-  }  
 }
--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,1 +1,1 @@
-Hello, World!
+Hello, feaking World!
